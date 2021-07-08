56f5feb8792b021ac03b68ba7819ae633b9788ae
Reece Rose <reecerose18@gmail.com>
2021-07-07 22:21:25 -0400

    Data Collector MVP (#2)
    * Added HTTP client and now sending hostname every X seconds
    
    * Added logging

---
+++ data-collector/cmd/data-collector/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/PR-Developers/server-health-monitor/data-collector/internal/client"
+	"github.com/PR-Developers/server-health-monitor/data-collector/internal/host"
+	"github.com/PR-Developers/server-health-monitor/data-collector/internal/logger"
+)
+
+type Host struct {
+	Hostname string
+}
+
+func main() {
+	log := logger.Instance()
+	log.Info("Server Health Monitor - Data Collector Tool")
+
+	host := host.GetInfo()
+	// TODO: Read from command line
+	var delay time.Duration = 30 // delay in seconds
+	payload := new(bytes.Buffer)
+	json.NewEncoder(payload).Encode(host)
+	client := client.NewClient()
+
+	for {
+		// Collect new data
+
+		// Make request
+		log.Info("Sending new health data")
+		_, statusCode, _ := client.Post("health", payload)
+		log.Info(fmt.Sprintf("Sent health data and got a status code of %v", statusCode))
+		// Delay for X seconds
+		time.Sleep(time.Second * delay)
+	}
+}
